ae5d919818af8c20bd3c6c03583f26bbb8d42366
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-12 12:34:02 +0530

    (feat) : create protected API view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.template.context_processors import request
 
+from config.app.views import HelloView
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request):
+        content = {'message': 'Hello, Rajashree'}
+        return Response(content)
+    
