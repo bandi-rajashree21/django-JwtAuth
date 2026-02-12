7100969e1081808effd590c9cd84cdcb7d854b45
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-12 12:53:02 +0530

    (chore) : add gitignore

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.template.context_processors import request
 
-from config.app.views import HelloView
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
