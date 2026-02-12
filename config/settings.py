12576c1ea97a175b5af82a6e4f989114e6eed2a3
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-12 12:28:25 +0530

    (feat) : add REST Framework JWT Configuration

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'rest_framework',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +99,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 
 # Internationalization
